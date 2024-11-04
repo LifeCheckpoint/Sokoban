3411ee50115c675f79ef5b968ee7787c5e216eca
LDDMiao <2435206827@qq.com>
2024-11-05 00:39:51 +0800

    尝试构建主界面

--- core/src/main/java/com/sokoban/Main.java
+++ core/src/main/java/com/sokoban/Main.java
@@ -2,33 +2,52 @@ package com.sokoban;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    FitViewport viewport;
+    private Texture startGameButtonText;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        viewport = new FitViewport(8, 5);
+        startGameButtonText = new Texture("start_game.png");
+    }
+
+    private void draw() {
+        ScreenUtils.clear(new Color(0xfffcdcff));
+        viewport.apply();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+
+        batch.draw(startGameButtonText, 0.1f, 0.8f, 2.5f, 0.5f);
+
+        batch.end();
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        // input();
+        // logic();
+        draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centers the camera
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        startGameButtonText.dispose();
     }
 }
