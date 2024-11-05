0580977481731fc1769f596940233bacbc300334
LDDMiao <2435206827@qq.com>
2024-11-05 17:55:50 +0800

    start按钮动效

--- core/src/main/java/com/sokoban/Main.java
+++ core/src/main/java/com/sokoban/Main.java
@@ -4,17 +4,19 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
-// import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-// import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
@@ -57,10 +59,22 @@ public class Main extends ApplicationAdapter {
         startGameButton = new ImageButton(new TextureRegionDrawable(startGameButtonRegion));
         startGameButton.setSize(2f, 3f);
         startGameButton.setPosition(2f, 2f);
+        startGameButton.setTransform(true);
         startGameButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 System.out.println("Start Game!");
+            }
+        });
+        startGameButton.addListener(new InputListener() {
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                startGameButton.addAction(Actions.scaleTo(1.2f, 1.2f, 0.2f, Interpolation.sine)); // 增大按钮
+            }
+
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                startGameButton.addAction(Actions.scaleTo(1f, 1f, 0.2f, Interpolation.sine)); // 恢复原始大小
             }
         });
 
--- lwjgl3/src/main/java/com/sokoban/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/sokoban/lwjgl3/Lwjgl3Launcher.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.sokoban.Main;
 
-/** Launches the desktop (LWJGL3) application. */
+/** Launches Sokoban. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
-        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        if (StartupHelper.startNewJvmIfRequired()) return;
         createApplication();
     }
 
