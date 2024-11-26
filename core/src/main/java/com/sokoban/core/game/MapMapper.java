dea61fd3824c645431cf30e988e0a4c6ac94894e
LDDMiao <2435206827@qq.com>
2024-11-26 15:20:35 +0800

    补充地图映射类映射

--- core/src/main/java/com/sokoban/core/game/MapMapper.java
+++ core/src/main/java/com/sokoban/core/game/MapMapper.java
@@ -1,53 +1,127 @@
 package com.sokoban.core.game;
 
+import com.sokoban.core.Logger;
+
 public class MapMapper {
-    public static ObjectType MapNumToType(int num) {
-        if (num == 0)
-            return ObjectType.AIR;
-        else if (num >= 1 && num <= 9) {
-            return ObjectType.WALL;
-        } else if (num == 10) {
-            return ObjectType.PLAYER;
-        } else if (num == 11) {
-            return ObjectType.PlayerGetBoxpos;
-        } else if (num == 12) {
-            return ObjectType.PlayerGetPLpos;
-        } else if (num == 20) {
-            return ObjectType.BOX;
-        } else if (num == 21) {
-            return ObjectType.BoxGetPLpos;
-        } else if (num == 30) {
-            return ObjectType.PosOfBox;
-        } else if (num == 31) {
-            return ObjectType.PosOfPL;
-        } else
-            return ObjectType.Unknown;
+    /**
+     * 映射地图元素到整数
+     * @param obj 地图元素
+     * @return 对应整数
+     */
+    public static int mapObjectTypeToNum(ObjectType obj) {
+        return switch (obj) {
+            case ObjectType.AIR -> 0;
+            case ObjectType.WALL -> 1;
+            case ObjectType.PLAYER -> 10;
+            case ObjectType.PlayerGetBoxpos -> 11;
+            case ObjectType.PlayerGetPLpos -> 12;
+            case ObjectType.BOX -> 20;
+            case ObjectType.BoxGetPLpos -> 21;
+            case ObjectType.PosOfBox -> 30;
+            case ObjectType.PosOfPL -> 31;
+            case ObjectType.BoxGetBoxpos -> 32;
+            case ObjectType.Unknown -> 100;
+        };
     }
 
     /**
-     * 映射字符到地图元素，这是为了与传统地图格式兼容
+     * 映射整数到地图元素
+     * @param num 整数
+     * @return 地图元素
+     */
+    public static ObjectType MapNumToType(int num) {
+        return switch (num) {
+            case 0 -> ObjectType.AIR;
+            case 1, 2, 3, 4, 5, 6, 7, 8, 9 -> ObjectType.WALL;
+            case 10 -> ObjectType.PLAYER;
+            case 11 -> ObjectType.PlayerGetBoxpos;
+            case 12 -> ObjectType.PlayerGetPLpos;
+            case 20 -> ObjectType.BOX;
+            case 21 -> ObjectType.BoxGetPLpos;
+            case 30 -> ObjectType.PosOfBox;
+            case 31 -> ObjectType.PosOfPL;
+            case 32, 33, 34, 35, 36, 37, 38, 39 -> ObjectType.BoxGetBoxpos;
+            default -> ObjectType.Unknown;
+        };
+    }
+
+    /**
+     * 映射字符到地图元素，与传统地图格式兼容
      * @param character 字符
      * @return 地图元素
      */
     public static ObjectType MapCharToType(char character) {
-        switch (character) {
-            case '_':
-            case '-':
-                return ObjectType.AIR;
-            case '#':
-                return ObjectType.WALL;
-            case '.':
-                return ObjectType.PosOfBox;
-            case '$':
-                return ObjectType.BOX;
-            case '@':
-                return ObjectType.PLAYER;
-            case '*':
-                return ObjectType.PosOfBox; // 应该是已经放好的箱子
-            case '+':
-                return ObjectType.PlayerGetBoxpos;
-            default:
-                return ObjectType.Unknown;
+        return switch (character) {
+            case '_', '-' -> ObjectType.AIR;
+            case '#' -> ObjectType.WALL;
+            case '.' -> ObjectType.PosOfBox;
+            case '$' -> ObjectType.BOX;
+            case '@' -> ObjectType.PLAYER;
+            case '*' -> ObjectType.BoxGetBoxpos;
+            case '+' -> ObjectType.PlayerGetBoxpos;
+            default -> ObjectType.Unknown;
+        };
+    }
+
+    /**
+     * 映射地图元素到字符，与 传统地图格式兼容
+     * @param obj 地图元素
+     * @return 对应字符
+     */
+    public static char mapObjectToChar(ObjectType obj) {
+        return switch (obj) {
+            case ObjectType.AIR -> '_';
+            case ObjectType.WALL -> '#';
+            case ObjectType.PosOfBox -> '.';
+            case ObjectType.BoxGetBoxpos -> '*';
+            case ObjectType.BOX -> '$';
+            case ObjectType.PLAYER -> '@';
+            case ObjectType.PlayerGetBoxpos -> '+';
+            default -> '?';
+        };
+    }
+
+    /**
+     * 将字符地图转换为整数地图，与传统地图格式兼容
+     * @param charMap 字符纯地图
+     * @return 整数纯地图，失败返回 null
+     */
+    public static int[][] transformCharMapToIntegerMap(char[][] charMap) {
+        if (charMap == null) {
+            Logger.error("MapMapper", "Cannot convert a null char map object");
+            return null;
         }
+
+        int[][] intMap = new int[charMap.length][charMap[0].length];
+
+        for (int i = 0; i < intMap.length; i++) {
+            for (int j = 0; j < intMap[0].length; j++) {
+                intMap[i][j] = mapObjectTypeToNum(MapCharToType(charMap[i][j]));
+            }
+        }
+
+        return intMap;
+    }
+
+    /**
+     * 将整数地图转换为字符地图，与传统地图格式兼容
+     * @param intMap 整数纯地图
+     * @return 字符纯地图，失败返回 null
+     */
+    public static char[][] transformIntegerMapToCharMap(int[][] intMap) {
+        if (intMap == null) {
+            Logger.error("MapMapper", "Cannot convert a null char map object");
+            return null;
+        }
+
+        char[][] charMap = new char[intMap.length][intMap[0].length];
+
+        for (int i = 0; i < charMap.length; i++) {
+            for (int j = 0; j < charMap[0].length; j++) {
+                charMap[i][j] = mapObjectToChar(MapNumToType(intMap[i][j]));
+            }
+        }
+
+        return charMap;
     }
 }
--- core/src/main/java/com/sokoban/core/game/ObjectType.java
+++ core/src/main/java/com/sokoban/core/game/ObjectType.java
@@ -1,5 +1,15 @@
 package com.sokoban.core.game;
 
 public enum ObjectType {
-    AIR, WALL, PLAYER, PlayerGetBoxpos, PlayerGetPLpos, BOX, BoxGetPLpos, PosOfBox, PosOfPL, Unknown
+    AIR, // 空气
+    WALL, // 墙
+    PLAYER, // 玩家
+    PlayerGetBoxpos, // 处于箱子目标位置的玩家 
+    PlayerGetPLpos, // 处于玩家目标位置的玩家
+    BOX, // 箱子
+    BoxGetPLpos, // 处于玩家目标位置的箱子
+    BoxGetBoxpos, // 处于箱子目标位置的箱子
+    PosOfBox, // 箱子目标位置
+    PosOfPL, // 玩家目标位置
+    Unknown // 未知
 }
