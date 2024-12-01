f116a6aab48cacc2d3afba229fd600e6f2e5d361
LDDMiao <2435206827@qq.com>
2024-12-01 16:13:34 +0800

    解决MapData矛盾

--- core/src/main/java/com/sokoban/core/map/MapData.java
+++ core/src/main/java/com/sokoban/core/map/MapData.java
@@ -47,43 +47,4 @@ public class MapData {
 
         return true;
     }
-<<<<<<< HEAD
-
-    public void setAddtionInfo(String addtionInfo) {
-        this.addtionInfo = addtionInfo;
-    }
-
-    public List<Map> getMaps() {
-        return maps;
-    }
-
-    public void setMaps(List<Map> maps) {
-        this.maps = maps;
-    }
-
-    public String[] getMapReader(String levelName, String mapName) {
-        MapFileReader reader = new MapFileReader();
-        String mapContent = reader.readMapByLevelAndName("tutorial", "map1");
-        String[] lines = mapContent.split("\n");
-        for (int i = 0; i < lines.length; i++)
-            lines[i] = lines[i].trim();
-        return lines;
-    }
-
-
-    public void MapDataManager(MapData mapData) {
-        String[] Lines = mapData.getMapReader("tutorial", "map1");//这里应该要读取什么输入
-        this.addtionInfo = Lines[0];
-        this.mapSize = Integer.parseInt(Lines[1]);
-        List<Map> allmaps = new ArrayList<>();
-
-
-    }
-
-
-
-
-
-=======
->>>>>>> 21b519a4b3c1abf1d41b1f074aa03882cdbc9dbb
 }
