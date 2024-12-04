6bfb54f38f308e2b79c9aa40a5e969b573443c3b
LDDMiao <2435206827@qq.com>
2024-12-04 11:59:09 +0800

    新增死锁检测模块与文档

---
+++ core/src/main/java/com/sokoban/algo/DeadLockTest.java
@@ -0,0 +1,21 @@
+package com.sokoban.algo;
+
+import com.sokoban.core.logic.ObjectType;
+
+/**
+ * 地图死锁检测算法
+ * @author Life_Checkpoint
+ */
+public class DeadLockTest {
+
+    /**
+     * 判断地图中是否存在死锁箱
+     * <br><br>
+     * 算法概述详见文档
+     * @param objectMap
+     * @return
+     */
+    public static boolean lockTest(ObjectType[][] objectMap) {
+        return false;
+    } 
+}
